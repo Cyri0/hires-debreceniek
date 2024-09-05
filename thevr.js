f41065cb2bf7541ccbe6e6d4d21255653f366500
Cyri0 <nemestamas94@gmail.com>
2024-09-05 09:27:21 +0200

    tankcsapda mashup added

--- thevr.js
+++ thevr.js
@@ -1,1 +1,8 @@
 console.log("Hello TheVR!");
+
+let video_holder = document.createElement('div')
+video_holder.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/aRQNQtMkoRM?si=RL0HfZ9HEFvwdOYg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
+
+video_holder.classList.add('youtube-video-holder')
+
+document.body.appendChild(video_holder)
